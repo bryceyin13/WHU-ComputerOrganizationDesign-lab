4e97ea44bf813ab76b3337c51a91278d55a4d9ff
Bryce <leonardian@126.com>
2023-04-27 23:25:36 +0800

    Delete Single_Cycle directory

--- Single_Cycle/Single_Cycle/PCRelAddrAdder.v
+++
@@ -1,7 +0,0 @@
-module PCRelAddrAdder(OffsetIn,AddrIn,AddrOut);
-  input[31:0] OffsetIn; //from ImmGen
-  input[31:0] AddrIn;   //from PC
-  output[31:0] AddrOut; //to AddrMUX
-  assign AddrOut=AddrIn+OffsetIn;
-endmodule
-
